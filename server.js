cd7a3fbc912f048f922d6ae7d85b8c00449c3ad1
Spirit4449 <nischayp510@gmail.com>
2025-08-21 14:23:46 -0400

    test2

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Express test
+// Express
 const express = require("express");
 // HTTP
 const http = require("http");
@@ -19,11 +19,11 @@ const CryptoJS = require("crypto-js");
 const rateLimit = require("express-rate-limit");
 
 // Webpack
-// const webpack = require("webpack");
-// const webpackDevMiddleware = require("webpack-dev-middleware");
-// const webpackHotMiddleware = require("webpack-hot-middleware");
-// const config = require("./webpack.config.js");
-// const compiler = webpack(config);
+const webpack = require("webpack");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
+const config = require("./webpack.config.js");
+const compiler = webpack(config);
 
 // Mail
 require('dotenv').config();
@@ -34,14 +34,14 @@ sgMail.setApiKey(
   process.env.SEND_GRID
 );
 
-// app.use(
-//   webpackDevMiddleware(compiler, {
-//     publicPath: config.output.publicPath,
-//     stats: "minimal"
-//   })
-// );
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    stats: "minimal"
+  })
+);
 
-// app.use(webpackHotMiddleware(compiler));
+app.use(webpackHotMiddleware(compiler));
 
 app.set('trust proxy', 1);
 
